e3652102c82eeccb862d5a11ca8ffa5c6d76e85d
LeoCp <leonardocolodette0@gmail.com>
2016-05-07 15:11:19 -0300

    :rocket: :rocket:

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-/*code*/
+const express = require('express')
+,app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.post('/addPessoa', (req,res) => {
+
+});
+
+app.listen(3000, () => console.log("Rodando..."));
