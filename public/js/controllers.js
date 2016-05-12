1c5ca2ad14451f9afeb1a629f457fd0273603882
Leonardo <leonardocolodette0@gmail.com>
2016-05-12 13:56:16 -0300

    Alterações

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,19 +1,17 @@
 angular.module("app")
-  .controller("homeCtrl",function ($scope){
-
-  })
-
+  .controller("homeCtrl",function ($scope){})
   .controller("cadastroCtrl", function ($scope,$http){
 
-      $scope.addPessoa = (pessoa) =>{
+      $scope.addPessoa = (pessoa) => {
         $http.post('/addPessoa', pessoa);
         $scope.pessoa = "";
-      }
+      };
+      
   })
   .controller("pessoasCtrl", function ($scope, $http){
 
-    $http.get('/showPessoas')
-      .then(function(response) {
-        $scope.pessoas = response.data;
-      });
+    $http.get('/showPessoas').then((res) => {
+      $scope.pessoas = res.data;
+    });
+    
   });
