508fe7af1e389a652980aa2d94d3f95530fbfe6d
LeoCp <leonardocolodette0@gmail.com>
2016-05-14 14:42:46 -0300

    Alterações no server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 ,bodyParser = require("body-parser")
 ,mongoose = require("mongoose")
 ,pessoaModel = require('./models/pessoa')
+,port = process.env.PORT || 5000
 ,app = express();require("./models/config");
 
 app.use('/static',  express.static(__dirname + '/node_modules'));
@@ -10,46 +11,41 @@ app.use('/view',  express.static(__dirname + '/public/views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const callback = (err,doc) => {
+  if (err) console.error('ERRPO: ' + err);
+};
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post('/addPessoa', (req,res) => {
-
   pessoaModel.findOne({nome:req.body.nome}, (err,data) => {
-    if (err) return console.error(err);
-
-    if(data){
-      console.log("Pessoa ja cadastrada!");
+    if (err) {
+      return console.error(err);
     }else{
-
-      const dataModel = {
-        nome: req.body.nome,
-        cpf: req.body.cpf,
-        idade: req.body.idade
-      },pessoa = new pessoaModel(dataModel);
-
-      pessoa.save((err, pessoa) => {
-        if (err) return console.error('ERRPO:',err);
-      });
-
+      if(data){
+        console.log("Pessoa ja cadastrada!");
+      }else{
+        const dataModel = {
+          nome: req.body.nome,
+          cpf: req.body.cpf,
+          idade: req.body.idade
+        },pessoa = new pessoaModel(dataModel);
+        pessoa.save(callback);
+      }
     }
   });
-
-
 });
 
 app.get('/showPessoas',(req,res) => {
-
   pessoaModel.find({}, (err,data) => {
     if (err){
       console.error(err);
     }else{
       res.json(data);
     }
-
   });
-
 });
 
 app.put('/alterarPessoa/:id', (req,res) => {
@@ -59,23 +55,13 @@ app.put('/alterarPessoa/:id', (req,res) => {
     cpf: req.body.cpf,
     idade: req.body.idade
   };
-
-  pessoaModel.update(query, mod, function (err,doc) {
-      if (err) console.log(err);
-  });
-
+  pessoaModel.update(query, mod, callback);
 });
 
 app.delete('/removePessoa/:id',(req,res) => {
   const query = {_id:req.params.id};
-  console.log(query);
-
-  pessoaModel.remove(query,function (err,doc) {
-    if (err) console.log(err);
-  });
+  pessoaModel.remove(query,callback);
 
 });
 
-
-
-app.listen(3000, () => console.log("Rodando..."));
+app.listen(port,() => console.log( "Rodando.."));
