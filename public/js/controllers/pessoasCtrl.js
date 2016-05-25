326431fa18976f32c0ec17a2a2ed6026a01c2cb1
LeoCp <leonardocolodette0@gmail.com>
2016-05-25 20:32:57 -0300

    pag

--- public/js/controllers/pessoasCtrl.js
+++ public/js/controllers/pessoasCtrl.js
@@ -2,8 +2,11 @@ angular.module("app")
   .controller("pessoasCtrl", function ($scope,$window,pessoasAPI){
 
     pessoasAPI.getPessoas().then((res) => {
+      pessoas = res.data;
       $scope.pessoas = res.data;
+      if (quantPessoas > 5) $scope.pagination = true;
     });
+
 
     $scope.bAlterar = (pessoa) => {
       $scope.p = pessoa;
